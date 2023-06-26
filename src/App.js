1850b8392db42e5e58993f5cf31544d5012ea4d7
Yash Khanvilkar <yash@coincade.io>
2023-06-26 12:15:23 +0530

    inital commit for services

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import About from "./components/About/About";
-import Home from "./components/Home/Home";
-import Navigationbar from "./container/Header/Header";
-import Footer from "./container/Footer/Footer";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Navigationbar from './container/Header/Header';
+import Footer from './container/Footer/Footer';
 
 function App() {
   return (
     <>
-      <Navigationbar/>
+      <Navigationbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
 }
